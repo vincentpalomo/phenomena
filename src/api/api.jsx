665c11ee62fc909f1bb043bef4dcd649e7698926
Vincent Palomo <vincentpalomo@yahoo.com>
2023-01-25 22:52:05 -0600

    fetch all reports working and mapped through reports and comments all showing on website

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -4,6 +4,6 @@ export const APIURL = 'https://phenomena-gmx2.onrender.com/api';
 export const fetchAllReports = async () => {
   const res = await fetch(`${APIURL}/reports`);
   const json = await res.json();
-  console.log(json);
-  return json;
+  console.log('from api', json.reports);
+  return json.reports;
 };
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { fetchAllReports } from '../api/api';
 
 const Main = () => {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+  const [reports, setReports] = useState([]);
+
+  useEffect(() => {
+    fetchReports();
+  }, []);
+
+  const fetchReports = async () => {
+    try {
+      const report = await fetchAllReports();
+      setReports(report);
+      console.log(report);
+    } catch (error) {
+      console.error('error in fetchReports', error);
+    }
+  };
+
+  return (
+    <>
+      <h1 className='text-3xl font-bold underline'>Phenomena Reports!</h1>
+      <div>
+        {reports.map((report) => {
+          return (
+            <div key={report.id}>
+              <h3>Report: {report.title}</h3>
+              <div>
+                <p>Location: {report.location}</p>
+                <p>Description: {report.description}</p>
+              </div>
+              <div>
+                {report.comments.map((comment) => {
+                  return (
+                    <div>
+                      <h3>Comments:</h3>
+                      <p>{comment.content}</p>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 };
 
 export default Main;
