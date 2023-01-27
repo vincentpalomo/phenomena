e80ba8fd5b5a66a907a8fd6b7894204527db712f
Vincent Palomo <vincentpalomo@yahoo.com>
2023-01-26 18:21:44 -0600

    updated text for title

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,7 @@ const NavBar = () => {
   return (
     <div className='mx-auto flex h-16 max-w-screen-2xl items-center justify-between sm:px-6 lg:px-8'>
       <div className='flex items-center'>
-        <span className=' text-white'>phenomena</span>
+        <span className='text-white font-serif text-4xl'>phenomena</span>
       </div>
       <div className='flex flex-1 items-center justify-end'>
         <button className='inline-block rounded bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 p-[2px] hover:text-white focus:outline-none focus:ring active:text-opacity-75'>
