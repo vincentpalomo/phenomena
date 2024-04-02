3ccbfb10f314b61ed584e6cee1d179ef01749dd1
Vincent Palomo <vincentpalomo@yahoo.com>
2024-04-02 17:51:54 -0500

    updated closed reports styling

--- src/components/ExpiredReports.jsx
+++ src/components/ExpiredReports.jsx
@@ -21,35 +21,53 @@ const ExpiredReports = () => {
 
   return (
     <>
+      <div className="flex justify-evenly p-1 font-robotomono text-sm tracking-[4px]">
+        <Link to="/openreports" className="active:text-green-400">
+          open
+        </Link>
+        <Link to="/closedreports" className="active:text-green-400">
+          closed
+        </Link>
+      </div>
       <div>
         {reports.map((report) => {
           return (
             <div key={report.id}>
               {report.isExpired && (
-                <div className='animate-background rounded-xl bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-[length:400%_400%] p-1 shadow-xl transition [animation-duration:_6s] hover:shadow-md mb-2'>
-                  <div className='rounded-[10px] bg-white p-4 sm:p-4'>
-                    <div className='p-4'>
-                      <h3 className='text-xl font-medium text-gray-900'>
-                        {report.title}
-                      </h3>
-                      <p className='mt-1 text-gray-500'>{report.location}</p>
-                      <p className='mt-1 text-gray-700'>{report.description}</p>
-                      <p className='mt-3'>Report ID: {report.id}</p>
+                <div className="animate-background rounded-xl bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-[length:400%_400%] p-1 shadow-xl transition [animation-duration:_6s] hover:shadow-md mb-2 h-[20vh]">
+                  <div className="rounded-[10px] bg-black p-4 sm:p-2 h-full">
+                    {/* report body */}
+                    <div className="p-1 font-robotomono lowercase tracking-[4px] flex flex-col justify-center h-full">
+                      <h3 className="text-xl font-medium text-white">{report.title}</h3>
+                      <div className="flex justify-between items-center">
+                        <p className="text-xs text-gray-500">{report.location}</p>
+                        <p className="text-xs">Report ID: {report.id}</p>
+                      </div>
+                      <div className="flex items-center gap-4 mt-5">
+                        <p>description:</p>
+                        <p className="mt-1 text-sm  text-gray-200">{report.description}</p>
+                      </div>
                     </div>
-                    <div className='p-4'>
-                      <div className="my-1 flex items-center gap-4 before:h-px before:flex-1 before:bg-gray-300  before:content-[''] after:h-px after:flex-1 after:bg-gray-300  after:content-['']">
+                    {/* comments */}
+                    {/* <div className='p-2'>
+                      <div className="my-1 flex items-center gap-1 before:h-px before:flex-1 before:bg-gray-300  before:content-[''] after:h-px after:flex-1 after:bg-gray-300  after:content-['']">
                         Comments
                       </div>
                       {report.comments.map((comment) => {
                         return (
                           <div key={comment.id} className='p-1'>
-                            <p className='m-1 text-gray-700'>
-                              {comment.content}
-                            </p>
+                            <p className='m-1 text-gray-700'>{comment.content}</p>
                           </div>
                         );
                       })}
                     </div>
+                    <div className='p-2'>
+                      <Link to='/addcomment'>
+                        <button onClick={() => getReportId(report.id)} className='my-1 text-pink-500'>
+                          <span className='font-bold'>Reply to thread</span>
+                        </button>
+                      </Link>
+                    </div> */}
                   </div>
                 </div>
               )}
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -24,23 +24,31 @@ const Main = ({ setReportId }) => {
 
   return (
     <>
+      <div className="flex justify-evenly p-1 font-robotomono text-sm tracking-[4px]">
+        <Link to="/openreports" className="active:text-green-400">
+          open
+        </Link>
+        <Link to="/closedreports" className="active:text-green-400">
+          closed
+        </Link>
+      </div>
       <div>
         {reports.map((report) => {
           return (
             <div key={report.id}>
               {!report.isExpired && (
-                <div className='animate-background rounded-xl bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-[length:400%_400%] p-1 shadow-xl transition [animation-duration:_6s] hover:shadow-md mb-2 h-[20vh]'>
-                  <div className='rounded-[10px] bg-black p-4 sm:p-2 h-full'>
+                <div className="animate-background rounded-xl bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-[length:400%_400%] p-1 shadow-xl transition [animation-duration:_6s] hover:shadow-md mb-2 h-[20vh]">
+                  <div className="rounded-[10px] bg-black p-4 sm:p-2 h-full">
                     {/* report body */}
-                    <div className='p-1 font-robotomono lowercase tracking-[4px] flex flex-col justify-center h-full'>
-                      <h3 className='text-xl font-medium text-white'>{report.title}</h3>
-                      <div className='flex justify-between items-center'>
-                        <p className='text-xs text-gray-500'>{report.location}</p>
-                        <p className='text-xs'>Report ID: {report.id}</p>
+                    <div className="p-1 font-robotomono lowercase tracking-[4px] flex flex-col justify-center h-full">
+                      <h3 className="text-xl font-medium text-white">{report.title}</h3>
+                      <div className="flex justify-between items-center">
+                        <p className="text-xs text-gray-500">{report.location}</p>
+                        <p className="text-xs">Report ID: {report.id}</p>
                       </div>
-                      <div className='flex items-center gap-4 mt-5'>
+                      <div className="flex items-center gap-4 mt-5">
                         <p>description:</p>
-                        <p className='mt-1 text-sm  text-gray-200'>{report.description}</p>
+                        <p className="mt-1 text-sm  text-gray-200">{report.description}</p>
                       </div>
                     </div>
                     {/* comments */}
