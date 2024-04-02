e19dd7a99d66b40526aaffe55d918ff804aee194
Vincent Palomo <vincentpalomo@yahoo.com>
2024-04-02 17:11:56 -0500

    adjusted padding on main page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,22 +3,22 @@ import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <div className='flex items-center justify-between h-32 mx-auto max-w-screen-2xl sm:px-6 lg:px-5'>
-      <div className='flex items-center'>
-        <Link to='/openreports'>
-          <span className='font-pilowlava text-[128px] text-white'>phenomena</span>
+    <div className="flex items-center justify-between h-32 mx-auto sm:px-6 lg:px-5">
+      <div className="flex items-center">
+        <Link to="/openreports">
+          <span className="font-pilowlava text-[128px] text-white">phenomena</span>
         </Link>
       </div>
 
-      <div className='font-robotomono flex flex-col justify-center items-end text-[14px] tracking-[4px]'>
-        <Link to='/' className='active:text-green-400'>
-          <button className='text-[14px] tracking-[4px] hover:text-green-400 focus:text-green-400'>about</button>
+      <div className="font-robotomono flex flex-col justify-center items-end text-[14px] tracking-[4px]">
+        <Link to="/" className="active:text-green-400">
+          <button className="text-[14px] tracking-[4px] hover:text-green-400 focus:text-green-400">about</button>
         </Link>
-        <Link to='/openreports' className='active:text-green-400'>
-          <button className='text-[14px] tracking-[4px] hover:text-green-400 focus:text-green-400'>experiences</button>
+        <Link to="/openreports" className="active:text-green-400">
+          <button className="text-[14px] tracking-[4px] hover:text-green-400 focus:text-green-400">experiences</button>
         </Link>
-        <Link to='addreport'>
-          <button className='text-[14px] tracking-[4px] hover:text-green-400 focus:text-green-400'>add report</button>
+        <Link to="addreport">
+          <button className="text-[14px] tracking-[4px] hover:text-green-400 focus:text-green-400">add report</button>
         </Link>
       </div>
     </div>
