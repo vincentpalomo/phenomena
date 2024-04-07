5c024c46e535f1c588e268c992790a2d44fa40fb
Vincent Palomo <vincentpalomo@yahoo.com>
2024-04-07 13:13:23 -0500

    updated post styling

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -72,10 +72,10 @@ const Posts = ({ reportId, setReportId }) => {
           </Link> */}
           <div className='h-[75vh] flex flex-col justify-evenly lowercase'>
             <div>
-              <div className='flex justify-between font-robotomono text-xs tracking-[4px] border-b-2 border-b-white '>
+              <div className='flex justify-between font-robotomono text-xs tracking-[4px] border-b border-b-white '>
                 <div className='py-2 px-1'>
                   <div className='text-5xl font-robotomono tracking-[3px] pb-2'>{report?.title}</div>
-                  <div className='pl-2'>{report?.location}</div>
+                  {/* <div className='ml-10'>{report?.location}</div> */}
                 </div>
                 <div className='flex flex-col justify-end items-end pb-2'>
                   <div className='pb-2'>
@@ -90,11 +90,12 @@ const Posts = ({ reportId, setReportId }) => {
               </div>
             </div>
 
-            <div className='px-10'>
+            <div className='px-10 relative'>
+              <div className='absolute -top-16 right-0 font-robotomono tracking-[4px] text-xs'>{report.location}</div>
               {/* <p className="font-robotomono text-sm tracking-[4px]">description:</p> */}
               <div className='font-robotomono text-sm tracking-[4px]'> {report?.description}</div>
             </div>
-            <div className='border-b-2'>
+            <div className='border-b'>
               {/* {report?.isExpired ? (
             <div className="text-red-500 font-robotomono text-xs tracking-[4px] border-b-2 border-t-2 p-2">closed</div>
           ) : (
@@ -119,7 +120,7 @@ const Posts = ({ reportId, setReportId }) => {
               {report.comments &&
                 report.comments.map((comment) => {
                   return (
-                    <div key={comment.id} className='flex justify-between border-b-2 pl-4 mb-5'>
+                    <div key={comment.id} className='flex justify-between  pl-10 mb-5'>
                       <p className='w-[70vw]'>{comment.content}</p>
                       <p className='text-xs'>comment id: {comment.id}</p>
                     </div>
