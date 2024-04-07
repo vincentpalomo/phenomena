12ac680bbf1c2a4f538116de823515862d948299
Vincent Palomo <vincentpalomo@yahoo.com>
2024-04-07 15:28:00 -0500

    updated styling in post

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -32,7 +32,6 @@ const Posts = ({ reportId, setReportId }) => {
     setReportId(reportId);
   };
 
-  // useEffect(() => {
   //   const storedReportId = localStorage.getItem('reportId');
   //   if (storedReportId) {
   //     console.log(storedReportId);
@@ -70,40 +69,32 @@ const Posts = ({ reportId, setReportId }) => {
           {/* <Link to='/reports' className='tracking-[4px] font-robotomono text-sm hover:text-green-500'>
             back
           </Link> */}
-          <div className='h-[75vh] flex flex-col justify-evenly lowercase'>
-            <div>
-              <div className='flex justify-between font-robotomono text-xs tracking-[4px] border-b border-b-white '>
-                <div className='py-2 px-1'>
-                  <div className='text-5xl font-robotomono tracking-[3px] pb-2'>{report?.title}</div>
-                  {/* <div className='ml-10'>{report?.location}</div> */}
+          <div className='h-[75vh] flex flex-col justify-start lowercase'>
+            <div className='flex justify-between font-robotomono text-xs tracking-[4px] border-b border-b-white'>
+              <div className='py-2 px-1'>
+                <div className='text-5xl font-robotomono tracking-[3px] pb-2'>{report?.title}</div>
+                {/* <div className='ml-10'>{report?.location}</div> */}
+              </div>
+              <div className='flex flex-col justify-end items-end pb-2 w-[20vw]'>
+                <div className='pb-2'>
+                  {report?.isExpired ? (
+                    <div className='text-red-500 font-robotomono text-xs tracking-[4px]'>closed</div>
+                  ) : (
+                    <div className='text-green-500 font-robotomono text-xs tracking-[4px]'>open</div>
+                  )}
                 </div>
-                <div className='flex flex-col justify-end items-end pb-2'>
-                  <div className='pb-2'>
-                    {report?.isExpired ? (
-                      <div className='text-red-500 font-robotomono text-xs tracking-[4px]'>closed</div>
-                    ) : (
-                      <div className='text-green-500 font-robotomono text-xs tracking-[4px]'>open</div>
-                    )}
-                  </div>
-                  <div>report id: {report?.id}</div>
-                </div>
+                <div>report id: {report?.id}</div>
               </div>
             </div>
 
-            <div className='px-10 relative'>
-              <div className='absolute -top-16 right-0 font-robotomono tracking-[4px] text-xs'>{report.location}</div>
+            <div className='px-10 relative flex justify-start items-center h-[20vh] border-b'>
+              <div className='absolute top-0 right-0 font-robotomono tracking-[4px] text-xs'>{report.location}</div>
               {/* <p className="font-robotomono text-sm tracking-[4px]">description:</p> */}
               <div className='font-robotomono text-sm tracking-[4px]'> {report?.description}</div>
             </div>
-            <div className='border-b'>
-              {/* {report?.isExpired ? (
-            <div className="text-red-500 font-robotomono text-xs tracking-[4px] border-b-2 border-t-2 p-2">closed</div>
-          ) : (
-            <div className="text-green-500 font-robotomono text-xs tracking-[4px] border-b-2 border-t-2 p-2">open</div>
-          )} */}
-            </div>
+
             <div className='font-robotomono tracking-[4px] text-sm'>
-              <div className='flex justify-between mb-3'>
+              <div className='flex justify-between my-5'>
                 <p className='pb-2'>Comments:</p>
                 <div>
                   {!report?.isExpired && (
@@ -117,7 +108,7 @@ const Posts = ({ reportId, setReportId }) => {
                   )}
                 </div>
               </div>
-              {report.comments &&
+              {/* {report.comments &&
                 report.comments.map((comment) => {
                   return (
                     <div key={comment.id} className='flex justify-between  pl-10 mb-5'>
@@ -125,7 +116,19 @@ const Posts = ({ reportId, setReportId }) => {
                       <p className='text-xs'>comment id: {comment.id}</p>
                     </div>
                   );
-                })}
+                })} */}
+              {report.comments && report.comments.length > 0 ? (
+                report.comments.map((comment) => {
+                  return (
+                    <div key={comment.id} className='flex justify-between  pl-10 mb-5'>
+                      <p className='w-[70vw]'>{comment.content}</p>
+                      <p className='text-xs'>comment id: {comment.id}</p>
+                    </div>
+                  );
+                })
+              ) : (
+                <p className='text-center'>No comments</p>
+              )}
             </div>
           </div>
         </div>
